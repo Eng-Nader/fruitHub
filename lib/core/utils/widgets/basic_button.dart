5d7e007aee9556994cb7ed58bf8057d40306f974
NaderSameer <nadersameer@std.mans.edu.eg>
2025-10-08 02:47:12 +0300

    Finihs of Basic Button

---
+++ lib/core/utils/widgets/basic_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fruithub/core/constants/constants.dart';
+import 'package:fruithub/core/utils/fruits_colors.dart';
+
+class BasicButton extends StatelessWidget {
+  const BasicButton({super.key, required this.onPressed, required this.title});
+  final VoidCallback onPressed;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          backgroundColor: FruitsColors.primaryColor,
+          fixedSize: const Size.fromHeight(54),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          )),
+      onPressed: onPressed,
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 16,
+          fontFamily: kFontFamily,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
--- lib/featurs/on_boarding/presentation/views/widgets/on_borading_body.dart
+++ lib/featurs/on_boarding/presentation/views/widgets/on_borading_body.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruithub/core/utils/fruits_colors.dart';
+import 'package:fruithub/core/utils/widgets/basic_button.dart';
 import 'package:fruithub/featurs/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoradingBody extends StatelessWidget {
@@ -9,6 +10,7 @@ class OnBoradingBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         const Expanded(
           child: OnBoradingPageView(),
@@ -22,6 +24,15 @@ class OnBoradingBody extends StatelessWidget {
             color: FruitsColors.primaryColor.withValues(
               alpha: .5,
             ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: BasicButton(
+            onPressed: () {},
+            title: 'التالي',
           ),
         )
       ],
